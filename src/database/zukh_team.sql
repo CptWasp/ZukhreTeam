825aa5701877c8e30e641ff1fdff2c2772c03063
Cpt Wasp <StigNelson11@gmail.com>
2020-06-15 15:36:53 +0300

    Всё готово

--- src/database/zukh_team.sql
+++ src/database/zukh_team.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Июн 14 2020 г., 14:49
+-- Время создания: Июн 15 2020 г., 15:36
 -- Версия сервера: 10.3.22-MariaDB
 -- Версия PHP: 7.1.33
 
@@ -44,7 +44,49 @@ CREATE TABLE `applications` (
 
 INSERT INTO `applications` (`application_id`, `auditorium`, `device`, `comment`, `date`, `status`, `user_id`, `tec_id`) VALUES
 (1, '203-5', 'Мышка', 'Перестала работать мышка на 4м компьютере', '2020-06-02', '1', 2, 1),
-(2, '3-305', 'Проектор', 'Перестал работать проектор, не могу показывать ребятам наши проекты', '2020-06-01', '1', 2, 2);
+(2, '3-305', 'Проектор', 'Перестал работать проектор, не могу показывать ребятам наши проекты', '2020-06-01', '0', 2, 2),
+(3, '203-5', 'утюг', 'не греет вообще и не брызгает вода', '2020-06-09', '2', 2, 1),
+(4, '3-203', 'Фильтр', 'Удлиннительный фильтр начал замыкать', '2020-06-06', '1', 1, 1),
+(5, '230-1', 'холодильник', 'морозильник перестал греть еду на столе', '2020-05-02', '0', 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `technics`
+--
+
+CREATE TABLE `technics` (
+  `tec_id` int(11) NOT NULL,
+  `tec_name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `technics`
+--
+
+INSERT INTO `technics` (`tec_id`, `tec_name`) VALUES
+(1, 'Какой-то мужик с улицы'),
+(2, 'Джеки Чан');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
+  `user_name` varchar(50) NOT NULL,
+  `profession` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `users`
+--
+
+INSERT INTO `users` (`user_id`, `user_name`, `profession`) VALUES
+(1, 'Ашрапов Азат Халилович', 'Профессор'),
+(2, 'Бахарева Ольга Владимировна', 'Директор института');
 
 --
 -- Индексы сохранённых таблиц
@@ -59,6 +101,20 @@ ALTER TABLE `applications`
   ADD KEY `tec_id` (`tec_id`);
 
 --
+-- Индексы таблицы `technics`
+--
+ALTER TABLE `technics`
+  ADD PRIMARY KEY (`tec_id`),
+  ADD KEY `tec_id` (`tec_id`);
+
+--
+-- Индексы таблицы `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
 -- AUTO_INCREMENT для сохранённых таблиц
 --
 
@@ -66,7 +122,19 @@ ALTER TABLE `applications`
 -- AUTO_INCREMENT для таблицы `applications`
 --
 ALTER TABLE `applications`
-  MODIFY `application_id` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `application_id` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT для таблицы `technics`
+--
+ALTER TABLE `technics`
+  MODIFY `tec_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT для таблицы `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Ограничения внешнего ключа сохраненных таблиц
